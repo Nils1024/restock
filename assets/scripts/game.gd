907c7d6e9f11660d77f70e9f739e413838e2ebf9
Nils <78668364+Nils1024@users.noreply.github.com>
2026-03-23 22:54:50 +0100

    Added a half rounded rectangle for the gui and some input validation for the game

--- assets/scripts/game.gd
+++ assets/scripts/game.gd
@@ -67,3 +67,11 @@ func is_near_water(map, x, y, min_val, max_val):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+# TODO: Look at the DDA/Bresenham algorithm
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("click"):
+		var mouse_pos = get_global_mouse_position()
+		
+		var cell = $TileMapLayer.local_to_map($TileMapLayer.to_local(mouse_pos))
+		print("Clicked:", cell)
