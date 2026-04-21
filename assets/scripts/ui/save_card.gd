036819fc7c581d83c5ed921aec75bf221378c9cf
Nils <78668364+Nils1024@users.noreply.github.com>
2026-04-21 23:55:23 +0200

    tried to fix the UI sizes

--- assets/scripts/ui/save_card.gd
+++ assets/scripts/ui/save_card.gd
@@ -1,10 +1,10 @@
-extends Control
+extends PanelContainer
 
 signal card_pressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$SaveButton.text = get_meta("Text", "Save")
+	$MarginContainer/VBoxContainer/Control/Label.text = get_meta("Text", "Save")
 
 func _on_save_button_pressed() -> void:
 	emit_signal("card_pressed")
